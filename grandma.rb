c06e02fe7d8c9ff60ec632fe500033955df9216c
Your Username <you@example.com>
2020-10-15 00:17:55 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,21 @@
+require 'pry'
 # Write a speak_to_grandma method.
+def speak_to_grandma(string)
+    if string != string.upcase 
+        return "HUH?! SPEAK UP, SONNY!"
+
+    
+
+    elsif string == "I LOVE YOU GRANDMA!"
+     
+        return "I LOVE YOU TOO PUMPKIN!"
+    elsif string == string.upcase 
+        return "NO, NOT SINCE 1938!"
+    
+    end 
+   
+end
+# binding.pry 
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
